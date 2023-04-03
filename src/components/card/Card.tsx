124fd5f44c285c7c3b75b279a78ef61cb848a024
tharindu-ascentic <tharindunc7@gmail.com>
2023-04-03 22:01:25 +0530

    UI Changes

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,19 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { padWithLeadingZeros } from '@/utils/helpers';
 // import './card.scss';
 
-const Card = ({ pokemon }: any) => {
+interface Pokemon {
+  id: number;
+  name: string;
+  image: string;
+}
+
+interface PokemonCard {
+  pokemon: Pokemon;
+  addToTeam: (pokemon: Pokemon) => void;
+}
+
+
+const Card = ({ pokemon, addToTeam }: PokemonCard) => {
     return (
         <div className="pokemon">
             <div className="picture">
-                <img className="img-fluid" src={pokemon.image} alt={pokemon.name} />
+                <img className="img-fluid" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} alt={pokemon.name} />
             </div>
             <div className="team-content">
                 <h3 className="name">#{padWithLeadingZeros(pokemon.id, 3)}</h3>
                 <h4 className="title">{pokemon.name}</h4>
             </div>
             <ul className="add-to-team">
-                <li><button className='text-button-color'> Add to Team </button></li>
+                <li><button className='text-button-color' onClick={() => addToTeam(pokemon)}> Add to Team </button></li>
             </ul>
         </div>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,19 @@ import Image from "next/image";
 
 import Card from "@/components/card/Card";
 
+interface Pokemon {
+  id: number;
+  name: string;
+  image: string;
+}
+
 export default function Home() {
   const pokeman = useSelector((state: any) => state.pokemon);
   const dispatch = useDispatch();
 
   const [sidebarOpen, setSidebarOpen] = useState(true);
   const [searchTerm, setSearchTerm] = useState('');
-
+  const [team, setTeam] = useState<Pokemon[]>([]);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
@@ -27,9 +33,27 @@ export default function Home() {
     setSearchTerm(event.target.value);
   }
 
-  const filteredPokemons = pokeman.pokemonList.filter((pokemon: any) =>
+  const filteredPokemons = pokeman.pokemonList.filter((pokemon: Pokemon) =>
     pokemon.name.toString().includes(searchTerm)
   );
+
+
+  const handleAddToTeam = (pokemon: Pokemon) => {
+    if (team.length < 6 && !team.includes(pokemon)) {
+      setTeam([...team, pokemon]);
+    }
+  };
+
+  const handleRemoveFromTeam = (pokemon: Pokemon) => {
+    setTeam(team.filter((p) => p !== pokemon));
+  };
+
+  const handleTeamOrderChange = (event: any, oldIndex: number, newIndex: number) => {
+    const newTeam = [...team];
+    newTeam.splice(oldIndex, 1);
+    newTeam.splice(newIndex, 0, team[oldIndex]);
+    setTeam(newTeam);
+  };
 
   return (
     <>
@@ -45,21 +69,22 @@ export default function Home() {
       <main>
         <div className="app">
           <header className="header">
-            <h1>Pokemon Search</h1>
+            <div className="header-container">
+              <h1 className="header-title">Pokedex</h1>
+              <div className="search-box">
+                <input type="text" placeholder="Search for a Pokemon..." onChange={handleSearch} />
+              </div>
+            </div>
+          </header>
+          <main className="main">
             <button className="sidebar-toggle" onClick={toggleSidebar}>
               {isSidebarOpen ? 'Close Sidebar' : 'Open Sidebar'}
             </button>
-          </header>
-          <main className="main">
-            <div className="search-box">
-              <input type="text" placeholder="Search for a Pokemon..." onChange={handleSearch} />
-              <button>Search</button>
-            </div>
             <div className="pokemon-list">
               <div className="flex-container">
                 {filteredPokemons.map((pokemon: any, key: number) => (
                   <div className="flex-item" key={key}>
-                    <Card pokemon={pokemon} />
+                    <Card pokemon={pokemon} addToTeam={handleAddToTeam} />
                   </div>
                 ))}
               </div>
@@ -70,8 +95,26 @@ export default function Home() {
             onClick={toggleSidebar}
           >
             <div className="sidebar">
-              <h2>Filters</h2>
-              {/* Add your filter options here */}
+              <div className="container">
+                <img
+                  alt="Official Pokemon Logo"
+                  className="pokemon-logo"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png"
+                />
+                <div className="pokemon-list">
+                  {team.map((pokemon: any, key: number) => (
+                    <div className="pokemon-item" key={key}>
+                      <div className="pokemon-image">
+                        <img alt={pokemon.name} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} />
+                      </div>
+                      <div className="pokemon-detail">
+                        <span className="pokemon-detail__id">#001</span>
+                        <span className="pokemon-detail__name">{pokemon.name}</span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>

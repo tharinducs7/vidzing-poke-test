5769508be499a9b0f376d39d40f5185886e56c24
tharindu-ascentic <tharindunc7@gmail.com>
2023-04-03 12:14:24 +0530

    search functionality done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,24 @@ export default function Home() {
   const pokeman = useSelector((state: any) => state.pokemon);
   const dispatch = useDispatch();
 
-  console.log(pokeman.pokemonList, "pokeman");
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  }
 
   useEffect(() => {
     dispatch(getPokemonList());
   }, [dispatch]);
+
+  const handleSearch = (event: any) => {
+    setSearchTerm(event.target.value);
+  }
+
+  const filteredPokemons = pokeman.pokemonList.filter((pokemon: any) =>
+    pokemon.name.toString().includes(searchTerm)
+  );
 
   return (
     <>
@@ -29,11 +42,15 @@ export default function Home() {
       </Head>
       <main>
         <div>
-          <div className="header">Header Title</div>
+          <div className="header">Header Title
+          </div>
+           <div className="App-search">
+            <input type="text" placeholder="Search" onChange={handleSearch} />
+          </div>
           <div className="flex-container">
-            {pokeman.pokemonList.map((pokemon: any, key: number) => (
+            {filteredPokemons.map((pokemon: any, key: number) => (
               <div className="flex-item" key={key}>
-                <Card pokemon={pokemon}/>
+                <Card pokemon={pokemon} />
               </div>
             ))}
           </div>
